48fe6c9b65763de945ab8983ab75241b5c59e927
Veivneorul <thelightguardian@outlook.fr>
2024-07-30 10:39:23 +0200

    Ajout gestion de compte

--- account.php
+++ account.php
@@ -9,10 +9,22 @@ if (!isset($_SESSION['username'])) {
 
 require 'vendor/autoload.php';
 require 'php/api_config.php';
+require_once 'php/user_management.php';
 
-// Lire le fichier JSON
-$jsonData = file_get_contents('config.json');
-$data = json_decode($jsonData, true);
+$userInfo = getUserInfo($dbConfig, $_SESSION['username']);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = $_POST['email'];
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $password = $_POST['password'];
+
+    updateUserInfo($dbConfig, $_SESSION['username'], $email, $first_name, $last_name, $password);
+    // Update the $userInfo variable after the update so the form displays updated values
+    $userInfo = getUserInfo($dbConfig, $_SESSION['username']);
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -45,3 +57,43 @@ $data = json_decode($jsonData, true);
         </div>
     </div>
 </nav>
+
+<div class="container my-3">
+    <form method="post">
+        <!-- Username -->
+        <div class="mb-3">
+            <label for="username" class="form-label">Nom d'utilisateur</label>
+            <input type="username" class="form-control" id="username" name="username" value="<?php echo $userInfo['username'] ?>" disabled>
+        </div>
+        <!-- Email -->
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" class="form-control" id="email" name="email" value="<?php echo $userInfo['email'] ?>">
+        </div>
+        <!-- First Name -->
+        <div class="mb-3">
+            <label for="first_name" class="form-label">Prénom</label>
+            <input type="text" class="form-control" id="first_name" name="first_name" value="<?php echo $userInfo['first_name'] ?>">
+        </div>
+        <!-- Last Name -->
+        <div class="mb-3">
+            <label for="last_name" class="form-label">Nom de famille</label>
+            <input type="text" class="form-control" id="last_name" name="last_name" value="<?php echo $userInfo['last_name'] ?>">
+        </div>
+        <!-- Password  -->
+        <div class='mb-3'>
+            <label class="form-label" for='new_password'>New password</label>
+            <input type='password' class="form-control" id='new_password' name='password'>
+        </div>
+        <!-- Confirm Password -->
+        <div class='mb-3'>
+            <label class="form-label" for='confirm_password'>Confirm New password</label>
+            <input type='password' class="form-control" id='confirm_password' name='confirm_password'>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Mettre à jour</button>
+    </form>
+</div>
+
+</body>
+</html>
---
+++ php/user_management.php
@@ -0,0 +1,24 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+function getUserInfo($dbConfig, $username){
+    $connection = getDbConnection($dbConfig);
+
+    $query = "SELECT * FROM users LEFT JOIN user_info ON users.username = user_info.username WHERE users.username=?";
+    $stmt = $connection->prepare($query);
+    $stmt->execute([$username]);
+    $userInfo = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    return $userInfo;
+}
+
+function updateUserInfo($dbConfig, $username, $email, $first_name, $last_name, $password){
+    $connection = getDbConnection($dbConfig);
+    $password = password_hash($password, PASSWORD_BCRYPT);
+
+    $updateQuery = "UPDATE users, user_info 
+                    SET users.email = ?, user_info.first_name = ?, user_info.last_name = ?, users.password = ?
+                    WHERE users.username = user_info.username
+                    AND users.username = ?";
+    $updateStmt = $connection->prepare($updateQuery);
+    $updateStmt->execute([$email, $first_name, $last_name, $password, $username]);
+}
