0f7651125574e87364c4ac2d48d76d157c3917b4
Veivneorul <thelightguardian@outlook.fr>
2024-07-30 14:53:22 +0200

    - Amélioration de l'affichage

--- gestion.php
+++ gestion.php
@@ -40,7 +40,8 @@ while ($row = $resultSites->fetch()) {
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <title>Gestion</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link href="styles.css" rel="stylesheet"/>
 </head>
 <body>
@@ -51,32 +52,21 @@ while ($row = $resultSites->fetch()) {
     <!-- Tabs -->
     <ul class="nav nav-pills my-4" id="adminTabs" role="tablist">
         <li class="nav-item" role="presentation">
-            <button class="nav-link" id="categories-tab" data-bs-toggle="pill" data-bs-target="#categories" type="button" role="tab" aria-controls="categories">Gérer les catégories</button>
+            <button class="nav-link" id="categories-tab" data-bs-toggle="pill" data-bs-target="#categories"
+                    type="button" role="tab" aria-controls="categories">Gérer les catégories
+            </button>
         </li>
         <li class="nav-item" role="presentation">
-            <button class="nav-link" id="sites-tab" data-bs-toggle="pill" data-bs-target="#sites" type="button" role="tab" aria-controls="sites">Gérer les sites</button>
+            <button class="nav-link" id="sites-tab" data-bs-toggle="pill" data-bs-target="#sites" type="button"
+                    role="tab" aria-controls="sites">Gérer les sites
+            </button>
         </li>
     </ul>
 
     <!-- Tabs content -->
     <div class="tab-content" id="adminTabsContent">
         <!-- Catégories tab -->
-        <div class="tab-pane fade" id="categories" role="tabpanel" aria-labelledby="categories-tab">            <div class="row">
-                <?php foreach($categories as $cat_id => $cat_name): ?>
-                    <div class="col-md-4">
-                        <div class="card mb-4">
-                            <div class="card-body">
-                                <h3 class="card-title"><?php echo $cat_name; ?></h3>
-                                <form method='post' action='php/remove_cat.php'>
-                                    <input type='hidden' name='cat_id' value='<?php echo $cat_id; ?>' />
-                                    <button type='submit' class='btn btn-danger'>Supprimer</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                <?php endforeach; ?>
-            </div>
-
+        <div class="tab-pane fade" id="categories" role="tabpanel" aria-labelledby="categories-tab">
             <!-- Formulaire pour ajouter une nouvelle catégorie -->
             <div class="card my-4">
                 <div class="card-body">
@@ -84,35 +74,55 @@ while ($row = $resultSites->fetch()) {
                     <form class="mt-3" method="post" action="php/add_cat.php">
                         <div class="mb-3">
                             <label for="cat_name" class="form-label">Nom de la catégorie</label>
-                            <input type="text" class="form-control" name="cat_name" id="cat_name" placeholder="Nom de la nouvelle catégorie"/>
+                            <input type="text" class="form-control" name="cat_name" id="cat_name"
+                                   placeholder="Nom de la nouvelle catégorie"/>
                         </div>
                         <button type="submit" class="btn btn-primary">Ajouter une catégorie</button>
                     </form>
                 </div>
             </div>
 
-            <!-- Ajout d'une section pour modifier une catégorie existant -->
+            <!-- Ajout d'une section pour modifier une catégorie existante -->
             <div class="card my-4">
                 <div class="card-body">
                     <h3 class="card-title">Modifier une catégorie</h3>
-                    <form class="mt-3" method="post" action="php/edit_cat.php">
+                    <form class="mt-3 row" method="post" action="php/edit_cat.php">
                         <!-- Select pour choisir la catégorie à modifier -->
-                        <div class="mb-3">
-                            <label for="edit_cat_id" class="form-label">Choisir la catégorie</label>
+                        <div class="mb-3 col-md-6">
+                            <label for="edit_cat_id" class="form-label h5 mb-3 border-bottom pb-1 border-primary">Choisir la catégorie</label>
                             <select name="cat_id" id="edit_cat_id" class="form-control">
+                                <option value="">Sélectionner une catégorie</option>
                                 <?php foreach($categories as $cat_id => $cat_name): ?>
                                     <option value="<?php echo $cat_id; ?>"><?php echo $cat_name; ?></option>
                                 <?php endforeach; ?>
                             </select>
                         </div>
                         <!-- Input pour entrer le nouveau nom -->
-                        <div class="mb-3">
-                            <label for="new_cat_name" class="form-label">Nouveau nom</label>
+                        <div class="mb-3 col-md-6">
+                            <label for="new_cat_name" class="form-label h5 mb-3 border-bottom pb-1 border-danger">Nouveau nom</label>
                             <input type="text" class="form-control" name="new_cat_name" id="new_cat_name" placeholder="Nouveau nom de la catégorie">
                         </div>
-                        <button type="submit" class="btn btn-primary">Modifier la catégorie</button>
+                        <div class="col-12 mt-3">
+                            <button type="submit" class="btn btn-primary">Modifier la catégorie</button>
+                        </div>
                     </form>
                 </div>
+            </div>
+
+            <div class="row">
+                <?php foreach ($categories as $cat_id => $cat_name): ?>
+                    <div class="col-md-4">
+                        <div class="card mb-4">
+                            <div class="card-body">
+                                <h3 class="card-title"><?php echo $cat_name; ?></h3>
+                                <form method='post' action='php/remove_cat.php'>
+                                    <input type='hidden' name='cat_id' value='<?php echo $cat_id; ?>'/>
+                                    <button type='submit' class='btn btn-danger'>Supprimer</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
             </div>
 
         </div>
@@ -123,32 +133,38 @@ while ($row = $resultSites->fetch()) {
             <div class="card my-4">
                 <div class="card-body">
                     <h3 class="card-title">Ajouter un site</h3>
-                    <form class="mt-4" method="post" action="php/add_sites.php">
-                        <div class="mb-3">
+                    <form class="mt-4 row" method="post" action="php/add_sites.php">
+                        <div class="col-md-3 mb-3">
                             <label for="site_title" class="form-label">Titre de la carte</label>
-                            <input type="text" class="form-control" name="site_title" id="site_title" placeholder="Titre du nouveau site">
+                            <input type="text" class="form-control" name="site_title" id="site_title"
+                                   placeholder="Titre du nouveau site">
                         </div>
-                        <div class="mb-3">
+                        <div class="col-md-3 mb-3">
                             <label for="site_url" class="form-label">URL du site</label>
-                            <input type="text" class="form-control" name="site_url" id="site_url" placeholder="URL du nouveau site">
+                            <input type="text" class="form-control" name="site_url" id="site_url"
+                                   placeholder="URL du nouveau site">
                         </div>
-                        <div class="mb-3">
+                        <div class="col-md-3 mb-3">
                             <label for="site_tag" class="form-label">Tag pour le site</label>
-                            <input type="text" class="form-control" name="site_tag" id="site_tag" placeholder="Tag pour le nouveau site">
+                            <input type="text" class="form-control" name="site_tag" id="site_tag"
+                                   placeholder="Tag pour le nouveau site">
                         </div>
-                        <div class="mb-3">
+                        <div class="col-md-3 mb-3">
                             <label for="site_desc" class="form-label">Description du site</label>
-                            <input type="text" class="form-control" name="site_desc" id="site_desc" placeholder="Description du nouveau site">
+                            <input type="text" class="form-control" name="site_desc" id="site_desc"
+                                   placeholder="Description du nouveau site">
                         </div>
-                        <div class="mb-3">
+                        <div class="col-md-6 mb-3">
                             <label for="cat_id" class="form-label">Catégorie</label>
                             <select name="cat_id" id="cat_id" class="form-control">
-                                <?php foreach($categories as $cat_id => $cat_name): ?>
+                                <?php foreach ($categories as $cat_id => $cat_name): ?>
                                     <option value="<?php echo $cat_id; ?>"><?php echo $cat_name; ?></option>
                                 <?php endforeach; ?>
                             </select>
                         </div>
-                        <button type="submit" class="btn btn-primary">Ajouter un site</button>
+                        <div class="col-12 mt-3">
+                            <button type="submit" class="btn btn-primary">Ajouter un site</button>
+                        </div>
                     </form>
                 </div>
             </div>
@@ -157,38 +173,50 @@ while ($row = $resultSites->fetch()) {
             <div class="card my-4">
                 <div class="card-body">
                     <h3 class="card-title">Modifier un site</h3>
-                    <form id="editSiteForm" class="mt-4" method="post" action="php/edit_sites.php">
+                    <form id="editSiteForm" class="mt-4 row" method="post" action="php/edit_sites.php">
                         <!-- Select pour choisir le site à modifier -->
-                        <div class="mb-3">
-                            <label for="edit_site_id" class="form-label">Choisir le site</label>
+                        <div class="col-md-6 mb-3">
+                            <label for="edit_site_id" class="form-label h5 mb-3 border-bottom pb-1 border-primary">Choisir
+                                le site</label>
                             <select name="site_id" id="edit_site_id" class="form-control">
-                                <?php foreach($sites as $site): ?>
+                                <option value="">Sélectionner un site</option>
+                                <?php foreach ($sites as $site): ?>
                                     <option value="<?php echo $site['site_id']; ?>"><?php echo $site['site_title']; ?></option>
                                 <?php endforeach; ?>
                             </select>
                         </div>
                         <!-- Inputs pour modifier les infos du site -->
-                        <div class="mb-3">
-                            <label for="new_site_title" class="form-label">Nouveau titre</label>
-                            <input type="text" class="form-control" name="new_site_title" id="new_site_title" placeholder="Nouveau titre du site">
+                        <div class="col-md-3 mb-3">
+                            <label for="new_site_title" class="form-label h5 mb-3 border-bottom pb-1 border-danger">Nouveau
+                                titre</label>
+                            <input type="text" class="form-control" name="new_site_title" id="new_site_title"
+                                   placeholder="Nouveau titre du site">
                         </div>
-                        <div class="mb-3">
-                            <label for="new_site_url" class="form-label">Nouvelle URL</label>
-                            <input type="text" class="form-control" name="new_site_url" id="new_site_url" placeholder="Nouvelle URL du site">
+                        <div class="col-md-3 mb-3">
+                            <label for="new_site_url" class="form-label h5 mb-3 border-bottom pb-1 border-danger">Nouvelle
+                                URL</label>
+                            <input type="text" class="form-control" name="new_site_url" id="new_site_url"
+                                   placeholder="Nouvelle URL du site">
                         </div>
-                        <div class="mb-3">
-                            <label for="new_site_desc" class="form-label">Nouvelle description</label>
-                            <input type="text" class="form-control" name="new_site_desc" id="new_site_desc" placeholder="Nouvelle description du site">
+                        <div class="col-md-3 mb-3">
+                            <label for="new_site_desc" class="form-label h5 mb-3 border-bottom pb-1 border-danger">Nouvelle
+                                description</label>
+                            <input type="text" class="form-control" name="new_site_desc" id="new_site_desc"
+                                   placeholder="Nouvelle description du site">
                         </div>
-                        <div class="mb-3">
-                            <label for="new_cat_id" class="form-label">Nouvelle catégorie</label>
+                        <!-- Select pour modifier la catégorie du site -->
+                        <div class="col-md-3 mb-3">
+                            <label for="new_cat_id" class="form-label h5 mb-3 border-bottom pb-1 border-danger">Nouvelle
+                                catégorie</label>
                             <select name="cat_id" id="new_cat_id" class="form-control">
-                                <?php foreach($categories as $cat_id => $cat_name): ?>
+                                <?php foreach ($categories as $cat_id => $cat_name): ?>
                                     <option value="<?php echo $cat_id; ?>"><?php echo $cat_name; ?></option>
                                 <?php endforeach; ?>
                             </select>
                         </div>
-                        <button type="submit" class="btn btn-primary">Modifier le site</button>
+                        <div class="col-12 mt-3">
+                            <button type="submit" class="btn btn-primary">Modifier le site</button>
+                        </div>
                     </form>
                 </div>
             </div>
@@ -197,21 +225,25 @@ while ($row = $resultSites->fetch()) {
             <div class="card my-4">
                 <div class="card-body">
                     <h3 class="card-title">Sites existants</h3>
-                    <?php foreach($sites as $site): ?>
-                    <div class="card mb-2">
-                        <div class="card-body">
-                            <h5 class="card-title"><?php echo $site['site_title']; ?></h5>
-                            <p><strong>URL: </strong><?php echo $site['site_url']; ?></p>
-                            <p><strong>Tag: </strong><?php echo $site['site_tag']; ?></p>
-                            <p><strong>Description: </strong><?php echo $site['site_desc']; ?></p>
-                            <p><strong>Catégorie: </strong><?php echo $categories[$site['cat_id']] ?? 'N/A'; ?></p>
-                            <form method='post' action='php/remove_sites.php'>
-                                <input type='hidden' name='site_id' value='<?php echo $site['site_id']; ?>' />
-                                <button type='submit' class='btn btn-danger'>Supprimer</button>
-                            </form>
-                        </div>
+                    <div class="row">
+                        <?php foreach ($sites as $site): ?>
+                            <div class="col-md-6 col-lg-4 mb-4">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title"><?php echo $site['site_title']; ?></h5>
+                                        <p><strong>URL: </strong><?php echo $site['site_url']; ?></p>
+                                        <p><strong>Tag: </strong><?php echo $site['site_tag']; ?></p>
+                                        <p><strong>Description: </strong><?php echo $site['site_desc']; ?></p>
+                                        <p><strong>Catégorie: </strong><?php echo $categories[$site['cat_id']] ?? 'N/A'; ?></p>
+                                        <form method='post' action='php/remove_sites.php'>
+                                            <input type='hidden' name='site_id' value='<?php echo $site['site_id']; ?>'/>
+                                            <button type='submit' class='btn btn-danger'>Supprimer</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endforeach; ?>
                     </div>
-                    <?php endforeach; ?>
                 </div>
             </div>
         </div>
@@ -222,7 +254,9 @@ while ($row = $resultSites->fetch()) {
 $conn = null; // fermeture de la connexion à la base de données
 ?>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+        crossorigin="anonymous"></script>
 
 <script>
     // Initialiser les données des sites
@@ -268,7 +302,7 @@ $conn = null; // fermeture de la connexion à la base de données
     });
 
     // call on page load
-    document.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener("DOMContentLoaded", function () {
         switchTab();
     });
 </script>
