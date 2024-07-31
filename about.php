a102ea8142f1d651fc2a51d2971e291f720115df
Noa Ledet <noa.ledet@proton.me>
2024-07-31 23:05:26 +0200

    Optimisation de code et résolutions de bug.

--- about.php
+++ about.php
@@ -21,7 +21,7 @@ $versions = json_decode($json, true);
 
 <head>
     <meta charset="UTF-8">
-    <title>About</title>
+    <title>A propos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
