9e6fb0def560bc69c26335ec23b98a7e6ec7c107
Noa Ledet <noa.ledet@proton.me>
2024-08-29 13:56:34 +0200

    Ajout d'un script de test de la disponibilité d'un site.

--- php/install_sql.php
+++ php/install_sql.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
  * Copyright (c) 2024 - Veivneorul. This work is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License (BY-NC-ND 4.0).
  */
 
