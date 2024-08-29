c2f9300f0e4cbd02333b16af860ac022e30670b9
Noa Ledet <noa.ledet@proton.me>
2024-08-29 12:10:59 +0200

    Ajout d'un script de test de la disponibilité d'un site.

--- script/site_status_checker.php
+++ script/site_status_checker.php
@@ -1,4 +1,9 @@
 <?php
+
+/**
+ * Copyright (c) 2024 - Veivneorul. This work is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License (BY-NC-ND 4.0).
+ */
+
 declare(strict_types=1);
 
 global $dbConfig;
